368d4dfd37166b43cc147297d24eabf4ecaba5ba
joniscbum <jonimancillas187@gmail.com>
2023-09-26 21:42:47 -0600

    upload card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import st from "./CardStyles.module.css";
 
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className={st.card}>
+      <div className={st.pokeimg}>
+        <img  src="/images/001.png" alt="bulbasaur" />
+      </div>
+      <div className={st.namepokemon}>
+        <p>Bulbasaur</p>
+      </div>
+      <div className={st.pokemontypes}>
+        <div className={st.typegrass}>grass</div>
+        <div className={st.typepoison}>poison</div>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
