d7ead73dac84524ba822c1e455ab6df9bba9d22c
Edgar Mancillas <emancillas04@gmail.com>
2023-09-26 22:48:30 -0600

    fix imports

--- src/app/pokemon/page.jsx
+++ src/app/pokemon/page.jsx
@@ -1,12 +1,8 @@
-import PokeLoader from '@/components/shared/PokeLoader'
 import React from 'react'
 
 const Pokemon = () => {
   return (
     <div className='flex items-center justify-center text-black'>
-        <div className={'absolute top-1/2'}>
-        <PokeLoader/>
-        </div>
     </div>
   )
 }
