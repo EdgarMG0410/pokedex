e415d09aaac4a4c0449b58bf7397c0ab9045a63f
joniscbum <jonimancillas187@gmail.com>
2023-09-26 22:09:00 -0600

    fix card

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Card from "@/components/Card";
+import React from "react";
+import st from "./ListStyles.module.css";
+
+
 
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <main className="flex justify-center items-center min-h-screen bg-pk-red">
+      <div className={st.header}>
+        <h1 >151 DEX</h1>
+      </div>
+      <section className={st.jk}>
+      <Card />
+      <Card />
+      <Card />
+      </section>
+    </main>
+  );
+};
 
-export default List
+export default List;
