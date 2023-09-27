57de77383ccb3dc91ff0de9bc18dcda3eaee1893
joniscbum <jonimancillas187@gmail.com>
2023-09-26 22:42:28 -0600

    fix

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -2,18 +2,22 @@ import Card from "@/components/Card";
 import React from "react";
 import st from "./ListStyles.module.css";
 
-
-
 const List = () => {
   return (
-    <main className="flex justify-center items-center min-h-screen bg-pk-red">
+    <main className={st.body}>
       <div className={st.header}>
-        <h1 >151 DEX</h1>
+        <h1>151 DEX</h1>
       </div>
-      <section className={st.jk}>
-      <Card />
-      <Card />
-      <Card />
+      <section className={st.pokelist}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
     </main>
   );
