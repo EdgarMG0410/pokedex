87ebb8baa177b30e85cb2a4b24e97b280c3a4692
Edgar Mancillas <emancillas04@gmail.com>
2023-09-30 14:30:50 -0600

    update changes

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,30 +2,33 @@ import React from "react";
 import st from "./CardStyles.module.css";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import Badges  from "../Badge"
+import Badges from "../Badge"
 import { fadeBottom, fadeIn } from "../../utils/animations";
+import Link from "next/link";
 
-const Card = ({pokemon}) => {
+const Card = ({ pokemon }) => {
   console.log(pokemon)
   return (
-    <motion.div
-    initial={'hidden'}
-    whileInView={'visible'}
-    variants={fadeBottom(0.7,0.2)}
-    className={st.card}>
-            <div className={st.namepokemon}>
-        <p>{pokemon.name}</p>
-      </div>
-      <div className={st.pokeimg}>
-        <Image src={pokemon.sprites.front_default} alt="bulbasaur" fill />
-      </div>
+    <Link href={`/pokemon/${pokemon.name}`}>
+      <motion.div
+        initial={'hidden'}
+        whileInView={'visible'}
+        variants={fadeBottom(0.7, 0.2)}
+        className={st.card}>
+        <div className={st.namepokemon}>
+          <p>{pokemon.name}</p>
+        </div>
+        <div className={st.pokeimg}>
+          <Image src={pokemon.sprites.front_default} alt="bulbasaur" fill />
+        </div>
 
-      <div className={st.pokemontypes}>
-      {pokemon.types.map((type, index) => (
-          <Badges key={index} className={type.type.name} text={type.type.name}/>
-        ))}
-      </div>
-    </motion.div>
+        <div className={st.pokemontypes}>
+          {pokemon.types.map((type, index) => (
+            <Badges key={index} className={type.type.name} text={type.type.name} />
+          ))}
+        </div>
+      </motion.div>
+    </Link>
   );
 };
 
